de65a11cab249b8fb0b3a6338d4128eab5cc76db
spencerriley620 <spencer.riley@student.nmt.edu>
2020-08-05 10:16:43 -0600

    testing and debugging

--- R/pacman.r
+++ R/pacman.r
@@ -2,7 +2,7 @@
 #'
 #' This function will create a pac-man residual plot for for regression analysis.
 #' @param x: domain of the data y: range of the data title: title of the plot
-#' @keywords regression
+#' @keywords regression visualization
 #' @export
 #' @examples
 #' pacman()
@@ -25,6 +25,7 @@ pacman <- function(x,y,title){
 	rmin 		<- min(residual, na.rm=TRUE)
 # 6 equal divisions
 	divs 		<- seq(round(rmin, 0), round(rmax, 0), len=6)
+	print(divs)
 	if(divs[6] == 0){
 		par(oma=c(1,1,1,1), cex=0.9)
 		divs 	<- seq(round(rmin, 1), round(rmax, 1), len=5)
@@ -36,7 +37,7 @@ pacman <- function(x,y,title){
 
 # Color Scheme for the rings
 	color1 <- "Yellow"
-    color2 <- "White"
+  color2 <- "White"
 	if (divs[1] != 0){
 # Plots the residual against an angular position
 		polar.plot(0, rp.type="s",labels="", point.col="Red",
@@ -63,9 +64,9 @@ pacman <- function(x,y,title){
 # Plots the residual against an angular position
 		polar.plot(0, rp.type="s",labels="", point.col="Red",
 			radial.lim=c(0, divs[6]),show.grid=TRUE, show.grid.labels=FALSE,
-			main= NA, show.radial.grid=FALSE, grid.col="black")
+			main=title, show.radial.grid=FALSE, grid.col="black")
 
-		mtext(title, side=3,line=1)
+		# mtext(title, side=3,line=1)
 
 		draw.circle(0, 0, radius=divs[6], col=color1)
 		draw.circle(0, 0, radius=divs[5], col=color2)
