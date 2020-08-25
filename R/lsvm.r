17da57a94362a54e10c57c4046276d5de1b9fbfc
PharaohCola13 <spencer.riley@student.nmt.edu>
2020-08-25 10:24:27 -0600

    updates to util and lsvm

--- R/lsvm.r
+++ R/lsvm.r
@@ -9,92 +9,87 @@
 #' @param train_size Fraction of total data that the SVM will train on
 #' @return Pac-Man SVM
 #' @keywords machine-learning visualization
-#' @import plotrix circlize
+#' @import plotrix
 #' @importFrom graphics par text mtext rect abline plot
-#' @importFrom stats coef lm nls resid predict sigma rstandard median
-#' @importFrom utils packageDescription
+#' @importFrom stats coef resid predict median
 #' @importFrom e1071 svm
+#' @importFrom pacviz linMap rad2deg svm.partition
 #' @export
-lsvm <- function(x,y,l, title, train_size=0.7) {
-    pre <- svm.partition(x,y,l, train_size)
-    train_dat <- pre$train_dat
-    test_dat  <- pre$test_dat
-    train_lab <- pre$train_lab
-    test_lab  <- pre$test_lab
+
+lsvm <- function(x,y,l, title, train_size=0.7, rand_state=sample(1:2^15, 1)) {
+
+    pre       <- svm.partition(x,y,l, train_size, rand_state)
+    train     <- pre$train
+    test      <- pre$test
     train_idx <- pre$train_idx
 
-    svmfit <- svm(x=train_dat, y=train_lab,type="C-classification", kernel="linear", scale=FALSE)
-    pred <- predict(svmfit, test_dat)
+    svmfit <- svm(x=train[1:2], y=train[3],type="C-classification", kernel="linear", scale=FALSE)
+    pred <- predict(svmfit, test[1:2])
 
-    plot(train_dat, pch=16, col =ifelse(l[train_idx]==1, "blue", "red"))
+    plot(x,y, pch=16, col=ifelse(l[1:length(l)]==sort(unique(train[3])[,1])[1], "blue", "red"))
 
     cf <- coef(svmfit)
 
     abline(-cf[1]/cf[3], -cf[2]/cf[3], col = "red")
 
 # plot margin and mark support vectors
-    abline(-(cf[1] + 1)/cf[3], -cf[2]/cf[3], col = "blue")
-    abline(-(cf[1] - 1)/cf[3], -cf[2]/cf[3], col = "blue")
+    abline(-(cf[1] + 1)/cf[3], -cf[2]/cf[3], col = "black", lty="dashed")
+    abline(-(cf[1] - 1)/cf[3], -cf[2]/cf[3], col = "black", lty="dashed")
 }
 
-pac.lsvm <- function(x,y,lab, title, axis_label, color1 = "lightpink", color2= "lightsteelblue", color3="plum") {
-  pre <- svm.partition(x,y,lab, 0.7)
-  train_dat <- pre$train_dat
-  test_dat  <- pre$test_dat
-  train_lab <- pre$train_lab
-  test_lab  <- pre$test_lab
+pac.lsvm <- function(x,y,lab, title, axis_label, rand_state=sample(1:2^15, 1), color1 = "lightpink", color2= "lightsteelblue", color3="plum") {
+  pre       <- svm.partition(x,y,lab, 0.7, rand_state)
+  train     <- pre$train
+  test      <- pre$test
   train_idx <- pre$train_idx
 
-  svmfit <- svm(x=train_dat, y=train_lab,type="C-classification", kernel="linear", scale=FALSE)
-  pred <- predict(svmfit, test_dat)
+  svmfit <- svm(x=train[1:2], y=train[3],type="C-classification", kernel="linear", scale=FALSE)
+  pred <- predict(svmfit, test[1:2])
   cf <- coef(svmfit)
 
   t <- linMap(x, 40, 320)
-  r <- y
+  r <- linMap(y,1, 0)
 
   # Angular axis label positions
   lp = seq.int(40, 320, length.out = 6)
   # Angular axis labels
   ln = rev(seq.int(round(min(x, na.rm = TRUE), 1), round(max(x, na.rm = TRUE),1), length.out = 6))
 
-
-  db_x <- train_dat$Avg.Temperature
+  db_x <- train[1]
   db_t <- rad2deg(atan(-cf[1]/cf[3] * db_x - cf[2]/cf[3])/db_x)
   db_r <- -cf[1]/cf[3] * db_t - cf[2]/cf[3]
 
   s1_r <- -(cf[1] + 1)/cf[3] * db_t - cf[2]/cf[3]
   s2_r <- -(cf[1] - 1)/cf[3] * db_t - cf[2]/cf[3]
-  print(s1_r)
 
   # s1_t <- rad2deg(atan((-(cf[1] + 1)/cf[3] * db_x - cf[2]/cf[3])/(db_x)))
   # s2_t <- rad2deg(rad2deg(atan((-(cf[1] - 1)/cf[3] * db_x - cf[2]/cf[3])/(db_x))))
 
-  rmax <- max(c(r), na.rm = TRUE)
-  rmin <- min(c(r), na.rm = TRUE)
   # 6 equal divisions
-  divs <- seq(floor(rmin), ceiling(rmax), len = 6)
-  n <- divs[6]/10
+  divl  <- seq(floor(min(c(y), na.rm = TRUE)), ceiling(max(c(y), na.rm = TRUE)), len = 4)
+  divs  <- seq(floor(min(c(r), na.rm = TRUE)), ceiling(max(c(r), na.rm = TRUE)), len = 4)
+  n <- divs[4]/10
 
   # Plots the residual against an angular position
   par(oma = c(0, 0, 3, 0), cex = 0.9)
-  polar.plot(0, labels = "", radial.lim = c(0, divs[6]), show.grid = FALSE, show.grid.labels = FALSE,show.radial.grid = FALSE)
+  polar.plot(0, labels = "", radial.lim = c(0, divs[4]), show.grid = FALSE, show.grid.labels = FALSE,show.radial.grid = FALSE)
   title(paste("\n\n", title, sep = ""), outer = TRUE)
   # Generates 'tick marks' for angular axis
   for (i in lp) {
-      polar.plot(c(0, divs[6] + n/2), c(i, i), lwd = 1, rp.type = "p", line.col = "Black",add = TRUE)
+      polar.plot(c(0, divs[4] + n/2), c(i, i), lwd = 1, rp.type = "p", line.col = "Black",add = TRUE)
   }
   # Generates angular labels (w/ units) and axis title
   for (i in 1:6) {
       text <- paste(sprintf("%.2f", round(ln[i], 1)), " ", sep="")
       if (is.element(i, 1:3)) {
-          arctext(text, middle = (lp[i] * pi)/(180), radius = divs[6] + n, clockwise = TRUE)
+          arctext(text, middle = (lp[i] * pi)/(180), radius = divs[4] + n, clockwise = TRUE)
       } else if (is.element(i, 4:6)) {
-          arctext(text, middle = (lp[i] * pi)/(180), radius = divs[6] + n, clockwise = FALSE)
+          arctext(text, middle = (lp[i] * pi)/(180), radius = divs[4] + n, clockwise = FALSE)
       }
   }
-  arctext(axis_label, middle = 0, radius = divs[6] + n, clockwise = TRUE)
+  arctext(axis_label, middle = 0, radius = divs[4] + n, clockwise = TRUE)
 
-  for (i in 6:1) {
+  for (i in 4:1) {
       if ((i%%2) == 0) {
           color <- "gold"
       } else {
@@ -102,29 +97,28 @@ pac.lsvm <- function(x,y,lab, title, axis_label, color1 = "lightpink", color2= "
       }
       draw.circle(0, 0, radius = divs[i], col = color)
       rlab <- mean(c(abs(divs[i + 1]), abs(divs[i])))
-      text(rlab, 0, labels = bquote(.(divs[i + 1])))
+      text(rlab, 0, labels = bquote(.(round(divl[i + 1], 2))))
   }
 
-  print(min(s1_r))
-  print(max(s1_r))
-  polar.plot(c(0, divs[6]), c(40, 40), lwd = 1, rp.type = "l", line.col = "black",
-        radial.lim = c(0, divs[6]), add = TRUE)
-  polar.plot(c(0, divs[6]), c(320,320), lwd = 1, rp.type = "l", line.col = "black",
-          radial.lim = c(0, divs[6]), add = TRUE)
-  polar.plot(s1_r,db_t, lwd = 1, rp.type = "l", line.col = "black", radial.lim = c(0, divs[6]), add = TRUE)
+  polar.plot(c(0, divs[4]), c(40, 40), lwd = 1, rp.type = "l", line.col = "black",
+        radial.lim = c(0, divs[4]), add = TRUE)
+  polar.plot(c(0, divs[4]), c(320,320), lwd = 1, rp.type = "l", line.col = "black",
+          radial.lim = c(0, divs[4]), add = TRUE)
+
+  polar.plot(s1_r,db_t, lwd = 1, rp.type = "l", line.col = "black", radial.lim = c(0, divs[4]), add = TRUE)
   # draw.sector(start.degree = max(db_t), end.degree = min(db_t), rou1 = 0, rou2 = divs[6],
   #     lty = "dashed", border = "Black", col=color1)
-  polar.plot(r,t, rp.type = "s", point.col="black", add=TRUE)
+  polar.plot(r,t, rp.type = "s", point.col=ifelse(lab[1:length(lab)]==sort(unique(train[3])[,1])[1], "blue", "red"), point.symbols=16, add=TRUE)
 }
 
-# fname       <- read.table(file="../../Precipitable-Water-Model/data/ml/ml_data.csv", sep=",", header=TRUE, strip.white=TRUE)
-#
-# suppressPackageStartupMessages(library(circlize))
+# fname <- read.table(file="https://raw.githubusercontent.com/physicsgoddess1972/Precipitable-Water-Model/master/data/ml/ml_data.csv", sep=",", header=TRUE, strip.white=TRUE)
+# #
+# # suppressPackageStartupMessages(library(circlize))
 # library(e1071); library(plotrix)
 # x <- fname[,2]
 # y <- fname[,3]
 #
 # l <- fname[,5]
-# lsvm(x,y, l, "Function Test")
-# paclsvm(x,y, l, "Function Test", "Axis Label")
+# lsvm(x,y, l, "Function Test", rand_state=1)
+# pac.lsvm(x,y, l, "Function Test", "Axis Label", rand_state=1)
 # warnings()
--- R/util.r
+++ R/util.r
@@ -8,35 +8,61 @@
 #' @param train_size Fraction of total data that the SVM will train on
 #' @return Pac-Man SVM
 #' @keywords machine-learning visualization
-#' @import plotrix circlize
-#' @importFrom graphics par text mtext rect abline plot
-#' @importFrom stats coef lm nls resid predict sigma rstandard median
-#' @importFrom utils packageDescription
-#' @importFrom e1071 svm
+#' @example
+#' data("cars")
+#' l <- sample(rep(c(0,1),length.out=length(cars$dist)/2),length(cars$dist), replace=TRUE)
+#' svm.partition(cars$dist,cars$speed,l, colnames(cars)[2], colnames(cars)[1], train_size=0.1, rand_state=2)
 #' @export
-
-svm.partition <- function(x,y,l, train_size){
-  train_idx <- sample(1:length(x), trunc(length(x)*train_size))
+svm.partition <- function(x,y,l, train_size=0.7, rand_state=sample(1:2^15, 1)){
+  set.seed(rand_state)
+  train_idx <- sample(1:length(x), trunc(length(x)*train_size), replace=FALSE)
   test_idx  <- (1:length(x))[-(train_idx)]
 
-  train_dat <- data.frame(x[train_idx],
-                          y[train_idx])
-  train_lab <- factor(gsub(2, -1, c(l[train_idx])))
+  train     <- data.frame(x[train_idx],
+                          y[train_idx],
+                          factor(c(l[train_idx])))
+  colnames(train) <- c("x", "y", "labels")
 
-  names(train_dat)[1] <- "Avg.Temperature"
-  names(train_dat)[2] <- "Avg.TPW"
+  test    <- data.frame(x[test_idx],
+                          y[test_idx],
+                        factor(c(l[test_idx])))
+  colnames(test) <- c("x", "y", "labels")
 
-  test_dat  <- data.frame(x[test_idx],
-                          y[test_idx])
-  test_lab  <- factor(c(gsub(2, -1, l[test_idx])))
 
-  names(test_dat)[1] <- "Avg.Temperature"
-  names(test_dat)[2] <- "Avg.TPW"
-  output <- list(train_dat=train_dat, train_lab=train_lab, test_dat=test_dat, test_lab=test_lab, train_idx=train_idx)
+  output <- list(train=train, test=test, train_idx=train_idx)
   return(output)
 }
-
+# data("cars")
+# l <- sample(rep(c(0,1),length.out=length(cars$dist)/2),length(cars$dist), replace=TRUE)
+# svm.partition(cars$dist,cars$speed,l, train_size=0.1, rand_state=2)
+#' @title Pac-Man SVM
+#'
+#' @description Conversion between radians and degrees
+#' @param x,y Numeric data
+#' @param l Numeric labels data
+#' @param train_size Fraction of total data that the SVM will train on
+#' @return Pac-Man SVM
+#' @keywords machine-learning visualization
+#' @export
 rad2deg <- function(rad) {(rad * 180) / (pi)}
-deg2rad <- function(deg) {(deg * pi) / (180)}
 
+#' @title Pac-Man SVM
+#'
+#' @description Conversion between degrees and radians
+#' @param x,y Numeric data
+#' @param l Numeric labels data
+#' @param train_size Fraction of total data that the SVM will train on
+#' @return Pac-Man SVM
+#' @keywords machine-learning visualization
+#' @export
+deg2rad <- function(deg) {(deg * pi) / (180)}
+#' @title Pac-Man SVM
+#'
+#' @description linear map
+#' @param x,y Numeric data
+#' @param l Numeric labels data
+#' @param train_size Fraction of total data that the SVM will train on
+#' @return Pac-Man SVM
+#' @keywords machine-learning visualization
+#' @export
 linMap  <- function(x,i, f) {(x - min(x))/max(x - min(x)) * (i - f) + f}
