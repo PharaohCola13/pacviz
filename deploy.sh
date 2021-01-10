5a22f88e44d92e50d023fe5d5731e56db6961cfa
PharaohCola13 <spencer.riley@student.nmt.edu>
2021-01-09 18:27:59 -0700

    updates to man pages and deployment

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,8 @@
 while getopts "p" opt; do
 	case "${opt}" in
     p)
-				# Rscript -e "attachment::att_amend_desc()"
 				Rscript -e "usethis::use_tidy_description()"
 				Rscript -e "devtools::check(document=TRUE, manual=TRUE, cran=TRUE, vignettes=TRUE)"
-        # Rscript -e "devtools::build_manual(pkg ='.', path='./paper')"
 				;;
   esac
 done
