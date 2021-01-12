cb92e067ee9ae7f032e69bc2bc9afbac9472c668
PharaohCola13 <spencer.riley@student.nmt.edu>
2021-01-12 09:22:55 -0700

    revisions for cran submission

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ while getopts "p" opt; do
     p)
 				Rscript -e "usethis::use_tidy_description()"
 				Rscript -e "devtools::check(document=TRUE, manual=TRUE, cran=TRUE, vignettes=TRUE)"
+				Rscript -e "devtools::build(manual=TRUE, vignettes=TRUE)"
 				;;
   esac
 done
